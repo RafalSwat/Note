9f6a61256c4d1ee622a76bf42d47ed8090498c5d
RafalSwat <rafal.swat@gmail.com>
2019-05-30 23:31:38 +0200

    Authentication complete

--- Note/ViewController.swift
+++ Note/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordtextField: UITextField!
+    @IBOutlet weak var LogRegButton: UIButton!
+    @IBOutlet weak var LogRegSegmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func LogRegSegmentControlSwitch(_ sender: Any) {
+        if LogRegSegmentControl.selectedSegmentIndex == 0 {
+            
+            LogRegButton.setTitle("Log In", for: UIControl.State.normal)
+        } else {
+            
+            LogRegButton.setTitle("Registration", for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func LogRegButtonPressed(_ sender: UIButton) {
+        
+        if emailTextField.text != "" && passwordtextField.text != "" {
+            
+            if LogRegSegmentControl.selectedSegmentIndex == 0 {
 
-
+                //Login segment
+                Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordtextField.text!) {
+                    (user, error) in
+                    
+                    if user != nil {
+                        // show some details from user
+                        
+                    } else {
+                        
+                        if let specificError = error?.localizedDescription {
+                            print(specificError)
+                        } else {
+                            print("Error:  can`t login!")
+                        }
+                    }
+                }
+            } else {
+                
+                //Signup segment
+                Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordtextField.text!) {
+                    (user, error) in
+                    
+                    if user != nil {
+                        // show some details from user
+                        
+                    } else {
+                        
+                        if let specificError = error?.localizedDescription {
+                            print(specificError)
+                        } else {
+                            print("Error:  can`t register!")
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
 }
 
